f8943d23aca70431a21335bd70fc436c42eddecc
Nisha More <nisham@callondoc.com>
2025-11-23 14:33:58 +0530

    monggoseStrict remove

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://fitnesstrack-ecjt.onrender.com/api/",
+ baseURL: "https://fitnesstrack-ecjt.onrender.com/api/",
  // baseURL: "http://localhost:8080/api/",
 });
 
--- server/index.js
+++ server/index.js
@@ -14,8 +14,18 @@ app.use(cors({
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true })); // for form data
+const connectDB = () => {
+  mongoose
+    .connect(process.env.MONGODB_URL)
+    .then(() => console.log("Connected to Mongo DB"))
+    .catch((err) => {
+      console.error("failed to connect with mongo");
+      console.error(err);
+    });
+};
 
 app.use("/api/user/", UserRoutes);
+
 // error handler
 app.use((err, req, res, next) => {
   const status = err.status || 500;
@@ -29,20 +39,11 @@ app.use((err, req, res, next) => {
 
 app.get("/", async (req, res) => {
   res.status(200).json({
-    message: "Hello developers from SAURAV",
+    message: "Hello developers from SAURAV MORE FITNESSTRACK App",
   });
 });
 
-const connectDB = () => {
-  mongoose.set("strictQuery", true);
-  mongoose
-    .connect(process.env.MONGODB_URL)
-    .then(() => console.log("Connected to Mongo DB"))
-    .catch((err) => {
-      console.error("failed to connect with mongo");
-      console.error(err);
-    });
-};
+
 
 const startServer = async () => {
   try {
